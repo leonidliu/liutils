0e9bdd919ec9624ca82f71ceee8bc3cf45725ab3
Leo Liu <leo@movementcooperative.org>
2020-12-08 18:50:49 -0500

    Add table2 and checks update

--- R/checks.R
+++ R/checks.R
@@ -6,9 +6,8 @@
 #'
 #' @export
 check_na <- function(dat) {
-  if (!(class(dat) %in% c("data.frame", "data.table", "tibble"))) {
-    stop("argument 'dataframe' not of class data.frame, data.table, or tibble")
+  if (!(class(dat) %in% c("data.frame", "data.table", "tbl", "tbl_df", "spec_tbl_df"))) {
+    stop("argument 'dataframe' not of class data.frame, data.table, tbl, or tbl_df")
   }
   sapply(dat, function(x) sum(is.na(x)))
 }
-
